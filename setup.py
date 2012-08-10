1d65755de2353a74ba2d6c17e4248899f1da3dca
Andre Anjos <andre.anjos@idiap.ch>
2012-08-10 16:33:51 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.anjos@idiap.ch>
+# Sex 10 Ago 2012 14:22:33 CEST
+
+from setuptools import setup, find_packages
+
+# The only thing we do in this file is to call the setup() function with all
+# parameters that define our package.
+setup(
+
+    name='bob.db.casia_fasd',
+    version='master',
+    description='CASIA Face Anti-Spoofing Database Access API for Bob',
+    url='http://github.com/bioidiap/bob.db.replay',
+    license='LICENSE.txt',
+    author_email='Andre Anjos <andre.anjos@idiap.ch>',
+    #long_description=open('doc/howto.rst').read(),
+
+    # This line is required for any distutils based packaging.
+    packages=find_packages(),
+
+    # Non-python data files to be shipped
+    package_data = {
+      'iris': [
+        'db/cross_valid.txt',
+        'db/cut_attack.txt',
+        'db/cut_warped_attack.txt',
+        'db/cut_warped_video_attack.txt',
+        'db/real.txt',
+        'db/video_attack.txt',
+        'db/warped_attack.txt',
+        ],
+      },
+
+    install_requires=[
+        "bob == master",  # base signal proc./machine learning library
+    ],
+
+    entry_points={
+      'bob.db': [
+        'casia = casia_fasd.db',
+        ]
+      },
+
+)
