8fc0e82d23fcea577f87e5c66cde5bb5a086b466
Andre Anjos <andre.dos.anjos@gmail.com>
2012-08-22 17:37:46 +0200

    Remove get/put file tests

--- xbob/db/casia_fasd/test.py
+++ xbob/db/casia_fasd/test.py
@@ -97,23 +97,7 @@ class FASDDatabaseTest(unittest.TestCase):
     from bob.db.script.dbmanage import main
     self.assertEqual(main('casia_fasd checkfiles --self-test'.split()), 0)
   
-  def test05_manage_get(self):
-
-    from bob.db.script.dbmanage import main
-
-    self.assertEqual(main('casia_fasd get --dry-run'.split()), 0)
-    self.assertEqual(main('casia_fasd get --version=bla --dry-run'.split()), 0)
-    self.assertEqual(main('casia_fasd get --version=1.3.4 --verbose --dry-run'.split()), 0)
-
-  def test06_manage_put(self):
-
-    from bob.db.script.dbmanage import main
-
-    self.assertEqual(main('casia_fasd put --dry-run tmp'.split()), 0)
-    self.assertEqual(main('casia_fasd put --version=bla --dry-run tmp'.split()), 0)
-    self.assertEqual(main('casia_fasd put --version=1.3.4 --verbose --dry-run tmp'.split()), 0)
-
-  def test07_manage_location(self):
+  def test05_manage_location(self):
 
     from bob.db.script.dbmanage import main
 
