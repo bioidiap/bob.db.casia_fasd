25a5d67f85d40924e43e553d9336d546c77aab97
Ivana Chingovska <ivana.chingovska@idiap.ch>
2014-03-06 11:21:22 +0100

    querying by multiple attributes, compatibility with the new antispoofing.utils interface

--- xbob/db/casia_fasd/spoofing.py
+++ xbob/db/casia_fasd/spoofing.py
@@ -52,12 +52,23 @@ class File(FileBase):
     return self.__f.is_real()
   is_real.__doc__ = FileBase.is_real.__doc__
 
+  def get_type(self):
+    return self.__f.get_type()
+  get_type.__doc__ = "Returns the type of attack for this sample"
+  
+  def get_quality(self):
+    return self.__f.get_quality()
+  get_quality.__doc__ = "Returns the quality of attack for this sample"
+
 
 class Database(DatabaseBase):
   __doc__ = long_description
 
   def __init__ (self, args=None):
-    self.__db = CasiaFASDDatabase()
+    if args is not None:
+      self.__db = CasiaFASDDatabase(args.foldsdir)
+    else: 
+      self.__db = CasiaFASDDatabase()
     self.__kwargs = {}
     if args is not None:
 
@@ -67,6 +78,17 @@ class Database(DatabaseBase):
         'fold_no': args.casia_fold_number,
       }
   __init__.__doc__ = DatabaseBase.__init__.__doc__
+
+  def set_foldsdir(self, foldsdir):    
+    """Sets the directory holding the cross validation protocol"""
+    self.__db.set_foldsdir(foldsdir) 
+  
+  def get_protocols(self):
+    # In the case of this DB, this method does not precisely return the protocols (there are None defined actually!)
+    return list(self.__db.types)
+    
+  def get_attack_types(self):
+    return list(self.__db.types)
 
   def create_subparser(self, subparser, entry_point_name):
 
@@ -80,11 +102,13 @@ class Database(DatabaseBase):
         description=desc,
         formatter_class=RawDescriptionHelpFormatter)
 
-    p.add_argument('--types', type=str, choices=self.__db.types, dest='casia_types', help='Defines the types of attack videos in the database that are going to be used (if not set return all types)')
+    p.add_argument('--types', type=str, choices=self.__db.types, dest='casia_types', nargs='+', help='Defines the types of attack videos in the database that are going to be used (if not set return all types)')
 
     p.add_argument('--qualities', type=str, choices=self.__db.qualities, dest='casia_qualities', help='Defines the qualities of attack videos in the database that are going to be used (if not set return all qualities)')
 
     p.add_argument('--fold-number', choices=(1,2,3,4,5), type=int, default=1, dest='casia_fold_number', help='Number of the fold (defaults to %(default)s)')
+    
+    p.add_argument('--foldsdir', type=str, dest='foldsdir', help='The directory where the cross-validation protocol files are stored. If not specified, the default cross-validation files are used')
 
     p.set_defaults(name=entry_point_name)
     p.set_defaults(cls=Database)
@@ -96,6 +120,11 @@ class Database(DatabaseBase):
     from .driver import Interface
     i = Interface()
     return "CASIA FASD database (%s)" % i.name()
+    
+  def short_name(self):
+    from .driver import Interface
+    i = Interface()
+    return i.name()  
 
   def version(self):
     from .driver import Interface
@@ -128,9 +157,28 @@ class Database(DatabaseBase):
     if not types: types = self.__db.types
     return types, qualities, self.__kwargs.get('fold_no', 1)
 
-  def get_train_data(self):
+  def get_clients(self, group=None):
+    if group == 'train':
+      objects = self.get_train_data()[0] # only the real access objects are enough to query the client ids
+    elif group == 'devel':
+      objects = self.get_devel_data()[0]  
+    elif group == 'test':
+      objects = self.get_test_data()[0]
+    else: 
+      objects = self.get_train_data()[0] + self.get_devel_data()[0] + self.get_test_data()[0]
+    
+    return list(set([obj.get_client_id() for obj in objects]))
+   
+  def get_enroll_data(self, group=None):
+    """Returns either all enrollment objects or enrollment objects for a specific group"""
+    raise RuntimeError("This dataset does not have enrollment data")
+   
+  def get_train_data(self, fold_no=0):
 
-    types, qualities, fold_no = self.__parse_arguments()
+    if fold_no == 0:
+      types, qualities, fold_no = self.__parse_arguments()
+    else:
+      types, qualities, _ = self.__parse_arguments()  
 
     _, trainReal   = self.__db.cross_valid_foldobjects(cls='real', fold_no=fold_no, qualities=qualities)
     _, trainAttack = self.__db.cross_valid_foldobjects(cls='attack', types=types, fold_no=fold_no, qualities=qualities)
@@ -138,10 +186,13 @@ class Database(DatabaseBase):
     return [File(f) for f in trainReal], [File(f) for f in trainAttack]
   get_train_data.__doc__ = DatabaseBase.get_train_data.__doc__
 
-  def get_devel_data(self):
+  def get_devel_data(self, fold_no=0):
     __doc__ = DatabaseBase.get_devel_data.__doc__
 
-    types, qualities, fold_no = self.__parse_arguments()
+    if fold_no == 0:
+      types, qualities, fold_no = self.__parse_arguments()
+    else:
+      types, qualities, _ = self.__parse_arguments()  
 
     develReal, _   = self.__db.cross_valid_foldobjects(cls='real', fold_no=fold_no, qualities=qualities)
 
@@ -150,10 +201,13 @@ class Database(DatabaseBase):
     return [File(f) for f in develReal], [File(f) for f in develAttack]
   get_devel_data.__doc__ = DatabaseBase.get_devel_data.__doc__
 
-  def get_test_data(self):
+  def get_test_data(self, fold_no=0):
     __doc__ = DatabaseBase.get_test_data.__doc__
 
-    types, qualities, _ = self.__parse_arguments()
+    if fold_no == 0:
+      types, qualities, fold_no = self.__parse_arguments()
+    else:
+      types, qualities, _ = self.__parse_arguments()  
 
     testReal = self.__db.objects(groups='test', cls='real', qualities=qualities)
     testAttack = self.__db.objects(groups='test', cls='attack', types=types, qualities=qualities)
@@ -162,10 +216,40 @@ class Database(DatabaseBase):
   get_test_data.__doc__ = DatabaseBase.get_test_data.__doc__
 
   def get_test_filters(self):
-    raise NotImplementedError("Test filters have not yet been implemented for this database")
+    return ('types','qualities')
+    #raise NotImplementedError("Test filters have not yet been implemented for this database")
 
-  def get_filtered_test_data(self, filter):
-    raise NotImplementedError("Test filters have not yet been implemented for this database")
+  def get_filtered_test_data(self, filter, fold_no=0):
+    real, attack = self.get_test_data(fold_no = fold_no)
+
+    if filter == 'types':
+      return {
+          'warped': (real, [k for k in attack if k.get_type() == 'warped']),
+          'cut': (real, [k for k in attack if k.get_type() == 'cut']),
+          'video': (real, [k for k in attack if k.get_type() == 'video']),
+          }
+    elif filter == 'qualities':
+      return {
+          'low': ([k for k in real if k.get_quality() == 'low'], [k for k in attack if k.get_quality() == 'low']),
+          'normal': ([k for k in real if k.get_quality() == 'normal'], [k for k in attack if k.get_quality() == 'normal']),
+          'high': ([k for k in real if k.get_quality() == 'high'], [k for k in attack if k.get_quality() == 'high']),
+          }
+      
+  def get_filtered_devel_data(self, filter, fold_no=0):
+    real, attack = self.get_devel_data(fold_no = fold_no)
+
+    if filter == 'types':
+      return {
+          'warped': (real, [k for k in attack if k.get_type() == 'warped']),
+          'cut': (real, [k for k in attack if k.get_type() == 'cut']),
+          'video': (real, [k for k in attack if k.get_type() == 'video']),
+          }
+    elif filter == 'qualities':
+      return {
+          'low': ([k for k in real if k.get_quality() == 'low'], [k for k in attack if k.get_quality() == 'low']),
+          'normal': ([k for k in real if k.get_quality() == 'normal'], [k for k in attack if k.get_quality() == 'normal']),
+          'high': ([k for k in real if k.get_quality() == 'high'], [k for k in attack if k.get_quality() == 'high']),
+          }  
 
   def get_all_data(self):
     __doc__ = DatabaseBase.get_all_data.__doc__
