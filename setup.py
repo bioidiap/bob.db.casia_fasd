9295920c8d7fbdae1f10d49da5e6f1bd37a04781
Manuel Guenther <manuel.guenther@idiap.ch>
2014-11-06 15:36:07 +0100

    Implemented versioning by version.txt

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@
 
 from setuptools import setup, find_packages
 
+# Define package version
+version = open("version.txt").read().rstrip()
+
 # The only thing we do in this file is to call the setup() function with all
 # parameters that define our package.
 setup(
 
     name='bob.db.casia_fasd',
-    version='2.0.0a0',
+    version=version,
     description='CASIA Face Anti-Spoofing Database Access API for Bob',
     url='http://pypi.python.org/pypi/xbob.db.casia_fasd',
     license='GPLv3',
